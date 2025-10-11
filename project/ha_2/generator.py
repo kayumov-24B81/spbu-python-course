4d392bb0ad48c1d9e2c757f5ec3bb4604b23b593
Airat Kayumov <st128100@student.spbu.ru>
2025-10-11 18:06:08 +0300

    Removed temporary code from generator.py file

--- project/ha_2/generator.py
+++ project/ha_2/generator.py
@@ -147,10 +147,3 @@ def collect(stream: Iterator[Any], collection_type: Type[Any] = list) -> Any:
         raise ValueError("Cannot collect from terminal operation result")
 
     return collection_type(stream)
-
-
-if __name__ == "__main__":
-    result = map(lambda x: x, 5)
-    result = list(result)
-    for v in result:
-        print(v)
